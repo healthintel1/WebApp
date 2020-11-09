03247530b73f9b66659839d03f1b2aaeb1fd2a39
mkdsrt10 <dmayank0@icloud.com>
2020-11-10 02:11:44 +0530

    Modal

--- src/components/waiting.js
+++ src/components/waiting.js
@@ -46,7 +46,7 @@ class Waiting extends React.Component {
                   date: this.props.Dated+"/"+this.props.Month,
                 })
             };
-          fetch("https://18.188.45.196:8080/algo", requestOptions)
+          fetch("https://cors-anywhere.herokuapp.com/http://18.188.45.196:8080/algo", requestOptions)
             .then(res=>{
               GetData()
                 .then(res => {
