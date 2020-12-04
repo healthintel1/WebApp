e333978346496e1b80169600fd6778c94e528501
mkdsrt10 <dmayank0@icloud.com>
2020-12-04 09:42:06 +0530

    P

--- src/components/prediction.js
+++ src/components/prediction.js
@@ -65,14 +65,14 @@ class Prediction extends React.Component {
             this.setState({
                 output: {
                     head: "Yellow", sub: "",
-                    body: "Stay home and take care of yourself. Call your medical provider if you get worse. Please provide another set of readings within four hours to repeat the analysis"
+                    body: "Continue to monitor your health and contact your health care provider for advice if you get new symptoms or if you have close contact with someone with lab-confirmed COVID-19."
                 }
             })
         } else if (r === "red") {
             this.setState({
                 output: {
                     head: "Red", sub: "Stay home and take care of yourself",
-                    body: "Urgent Medical Attention may be needed. Please call 911 or go to the emergency department"
+                    body: "According to the CDC guidelines, the analysis of the data you entered indicates immediate medical attention may be necessary. Please contact your healthcare provider to discuss next steps. If your healthcare provider is not available, contact 911 or go to the emergency department."
                 }
             })
         } else if (r === "orange") {
@@ -138,41 +138,15 @@ class Prediction extends React.Component {
                 {<BrowserView>
                     <div style={{display:"flex", "flex-wrap":"wrap"}}>
                         <p className={`${("w-80 ml5")} mt4 dib`} style={{"font-size":"72px", "font-weight":"500", color: this.state.output.head, "line-height":"1.6"}}>{this.state.output.head} Status</p>
-                         {/* <img className="dib pt3 w-40" src="https://i.ibb.co/j45jqDj/Screenshot-2020-06-15-at-10-40-39-PM.png"/>*/}
                     </div>
                 </BrowserView>}
                 { <MobileView>
                     <p className="ml5 mt4 mb0 w-80 f1" style={{"font-weight":"500", color: this.state.output.head, "line-height":"1.6"}}>{this.state.output.head} Status</p>
-                     {/* <img className="ml4 w-80" src="https://i.ibb.co/j45jqDj/Screenshot-2020-06-15-at-10-40-39-PM.png"/>*/}
                 </MobileView>}
                 <div>
                     <p className={`f3 dark-gray ml4 mt4 b ${(isMobile)?"w-80":""}`}>{this.state.output.sub}</p>
                     <p className={`f5 gray ml4 mr6 ${(isMobile)?"w-80":""}`} style={{"line-height":"1.5"}}>{this.state.output.body}</p>
                 </div>
-                {/*<div className="w-100 bt bb bw1 b--light-gray mt4">*/}
-                {/*    <p className={`f5 ${(isMobile)?"ml4":"ml5"} mt2 mb2 dark-gray dib`}>NEXT STEPS</p>*/}
-                {/*</div>*/}
-                {/*<div className={`${(isMobile)?"ml4":"ml5"} list-div mv3`}>*/}
-                {/*    <span>1</span>  Start to quarantine yourself*/}
-                {/*</div>*/}
-                {/*<div className={`${(isMobile)?"ml4":"ml5"} list-div mv3`}>*/}
-                {/*    <span>2</span>  Visit the nearest hospital to get tested further*/}
-                {/*</div>*/}
-                {/*<div className="w-100 bt bb bw1 b--light-gray mt3">*/}
-                {/*    <p className={`f5 ${(isMobile)?"ml4":"ml5"} mt2 mb2 dark-gray dib`}>LINKS</p>*/}
-                {/*</div>*/}
-                {/*<BrowserView>*/}
-                {/*    <div style={{margin:"auto"}} className="tc">*/}
-                {/*        <p className="mt3 f5 dim pointer ph3 pv3 mb2 b dib white bg-light-red">CALL THE DOCTOR</p>*/}
-                {/*        <p className="ml2 mt3 f5 dim pointer ph4 pv3 mb2 b dib white bg-gray">LINK TO CDC</p>*/}
-                {/*        <p className="ml2 mt3 f5 dim pointer ph3 pv3 mb2 b dib white" style={{"background":"rgb(206,211,255)"}}>DIRECTIONS TO ER</p>*/}
-                {/*    </div>*/}
-                {/*</BrowserView>*/}
-                {/*<MobileView>*/}
-                {/*    <p className="ml4 tc w-60 mt3 f5 dim pointer ph4 pv3 mb1 b dib white bg-light-red">CALL THE DOCTOR</p>*/}
-                {/*    <p className="ml4 tc w-60 mt2 f5 dim pointer ph4 pv3 mb1 b dib white bg-gray">LINK TO CDC</p>*/}
-                {/*    <p className="ml4 tc w-60 mt2 f5 dim pointer ph4 pv3 mb2 b dib white" style={{"background":"rgb(206,211,255)"}}>DIRECTIONS TO ER</p>*/}
-                {/*</MobileView>*/}
             </div>
             <div className="mt4 flex">
                 <Graph Dated={this.props.Dated} Month={this.props.Month} data={this.state.data}/>
